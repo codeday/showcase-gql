6a0ddb457c9825a992d9b449726b8c9c2ae15369
Tyler Menezes <tylermenezes@codeday.org>
2020-11-30 16:29:34 -0500

    Finalize support for subscriptions and uploads

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,44 @@
-import { ApolloServer } from 'apollo-server';
+import Express from 'express';
+import http from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import { graphqlUploadExpress } from 'graphql-upload';
+import ws from 'ws';
+import { execute, subscribe } from 'graphql';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { createSchema } from './schema';
 import { createContext } from './context';
 import config from './config';
 
 export default async function server(): Promise<void> {
+  const schema = await createSchema();
   const apollo = new ApolloServer({
-    schema: await createSchema(),
+    schema,
     context: createContext,
     playground: config.debug,
     introspection: true,
     uploads: false,
-    subscriptions: {
-      path: '/subscriptions',
-    },
   });
 
-  // eslint-disable-next-line no-console
-  apollo.listen({ port: config.port }).then(({ url }) => console.log(url));
+  const app = Express();
+  app.use(graphqlUploadExpress({ maxFileSize: 100 * 1024 * 1024, maxFiles: 3 }));
+  apollo.applyMiddleware({ app });
+
+  const server = http.createServer(app);
+
+  const wsServer = new ws.Server({
+    server,
+    path: '/graphql',
+  });
+
+  server.listen(config.port, () => {
+    useServer(
+      {
+        schema,
+        execute,
+        subscribe,
+      },
+      wsServer,
+    );
+    console.log(`Listening on http://0.0.0.0:${config.port}`);
+  });
 }
