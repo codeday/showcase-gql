a4355ca71bb805d0760ab2f883eee63f84ace976
Nikhil Garg <nikhil@gargmail.com>
2022-07-07 12:23:56 -0700

    fix nullability for photos

--- src/types/Photo.ts
+++ src/types/Photo.ts
@@ -16,27 +16,27 @@ export class Photo {
   @Field(() => String)
   url: string;
 
-  @Field(() => String)
-  urlLarge: string;
+  @Field(() => String, { nullable: true })
+  urlLarge?: string;
 
-  @Field(() => String)
-  urlMedium: string;
+  @Field(() => String, { nullable: true })
+  urlMedium?: string;
 
-  @Field(() => String)
-  urlSmall: string;
+  @Field(() => String, { nullable: true })
+  urlSmall?: string;
 
   @Field(() => String, { nullable: true })
   thanks?: string;
 
-  @Field(() => String, { nullable: true })
-  programId?: string;
+  @Field(() => String)
+  programId: string;
+
+  @Field(() => String)
+  eventId: string;
 
   @Field(() => String, { nullable: true })
-  eventId?: string;
+  regionId: string;
 
   @Field(() => String, { nullable: true })
-  regionId?: string;
-
-  @Field(() => String, { nullable: true })
-  eventGroupId?: string;
+  eventGroupId: string;
 }
