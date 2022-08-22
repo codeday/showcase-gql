31b087d63e5013d0a6a7c2b1f7c01a1c2e976ef0
Tyler Menezes <tylermenezes@codeday.org>
2022-08-22 16:50:21 -0700

    Add photo uploads

--- src/resolvers/PhotoMutation.ts
+++ src/resolvers/PhotoMutation.ts
@@ -3,6 +3,8 @@ import {
 } from 'type-graphql';
 import { PrismaClient } from '@prisma/client';
 import { Inject } from 'typedi';
+import Uploader from '@codeday/uploader-node';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { Photo } from '../types/Photo';
 import { Context } from '../context';
 import { ImportPhotoInput } from '../inputs/ImportPhotoInput';
@@ -11,6 +13,9 @@ import { ImportPhotoInput } from '../inputs/ImportPhotoInput';
 export class PhotoMutation {
   @Inject(() => PrismaClient)
   private readonly prisma: PrismaClient;
+
+  @Inject(() => Uploader)
+  private readonly uploader : Uploader;
 
   @Mutation(() => Boolean)
   async importPhotos(
@@ -28,4 +33,41 @@ export class PhotoMutation {
 
     return true;
   }
+
+  @Mutation(() => Photo)
+  async uploadPhoto(
+    @Ctx() { auth }: Context,
+    @Arg('upload', () => GraphQLUpload) upload: FileUpload,
+    @Arg('eventId', () => String) eventId: string,
+    @Arg('programId', () => String, { nullable: true }) programId?: string,
+    @Arg('regionId', () => String, { nullable: true }) regionId?: string,
+    @Arg('eventGroupId', () => String, { nullable: true }) eventGroupId?: string,
+    @Arg('thanks', () => String, { nullable: true }) thanks?: string,
+  ): Promise<Photo> {
+    if (!(auth.isGlobalAdmin || auth.isEventAdmin(eventId))) throw new Error('No permission to upload photos.');
+
+    const chunks = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const chunk of upload.createReadStream()) {
+      chunks.push(chunk);
+    }
+    const uploadBuffer = Buffer.concat(chunks);
+
+    // Upload an image
+    const result = await this.uploader.image(uploadBuffer, upload.filename || '_.jpg');
+    return <Photo><unknown> this.prisma.photo.create({
+      data: {
+        url: result.url,
+        urlLarge: result.urlResize.replace('{width}', '1920').replace('{height}', '1080'),
+        urlMedium: result.urlResize.replace('{width}', '800').replace('{height}', '600'),
+        urlSmall: result.urlResize.replace('{width}', '256').replace('{height}', '125'),
+        eventId,
+        eventGroupId,
+        regionId,
+        programId,
+        thanks,
+      },
+    });
+  }
+
 }
