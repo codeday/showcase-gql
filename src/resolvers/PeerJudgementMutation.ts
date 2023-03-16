5b403eae65e31c76ebcaf724abb5de87f50adc35
Lola <lola@egherman.com>
2023-03-15 19:18:32 -0500

    more peer judging

--- src/resolvers/PeerJudgementMutation.ts
+++ src/resolvers/PeerJudgementMutation.ts
@@ -15,15 +15,22 @@ export class PeerJudgementMutation {
     async peerJudgeProjects(
         @Ctx() { auth }: Context,
         @Arg('eventId') eventId: string,
-        @Arg('projects') projects: string[],
+        @Arg('projects', () => [String]) projects: string[],
     ): Promise<boolean> {
       if (!auth.username) throw new Error('Auth token does not include username');
       if (!auth.isEventParticipant(eventId)) throw new Error('You are not a member of any projects at this event!');
       if (await Container.get(PrismaClient).peerJudgement.findFirst({ where: { eventId, username: auth.username } })) {
-        throw new Error('You have already voted');
+        // throw new Error('You have already voted');
+        // This way people can submit new votes really easily. Probably a better way to handle this but this is simple
+        await Container.get(PrismaClient).peerJudgement.deleteMany({ where: { eventId, username: auth.username } });
       }
-      const dbProjects = await Container.get(PrismaClient).project.findMany({ where: { id: { in: projects } } });
+      const dbProjects = await Container.get(PrismaClient).project.findMany(
+        { where: { id: { in: projects } }, select: { eventId: true, members: true, id: true } },
+      );
       if (!dbProjects.every((p) => p.eventId === eventId)) throw new Error('eventId mismatch');
+      if (dbProjects.some((p) => p.members.some((m) => m.username === auth.username))) {
+        throw new Error('You cannot vote for your own project!');
+      }
       // CreateMany does not exist in this version of prisma
       dbProjects.forEach(async (project) => {
         await Container.get(PrismaClient).peerJudgement.create({
